be1298198e577638c580e7f9fc12b0df52b65b85
rosca_ioana <rosca.ioana11@gmail.com>
2020-10-12 15:41:31 +0300

    .

---
+++ src/main/java/ro/fastrackit/classroom/Professor.java
@@ -0,0 +1,12 @@
+package ro.fastrackit.classroom;
+
+import java.time.LocalDate;
+
+public class Professor {
+    private String lastName;
+    private String firstName;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfHiring;
+    private String emailAddress;
+    private Boolean isPhd;
+}
