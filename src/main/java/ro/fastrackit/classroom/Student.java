767c3adebd56c0153c96e2237382cd3dd8594b3c
rosca_ioana <rosca.ioana11@gmail.com>
2020-10-12 15:10:31 +0300

    .

---
+++ src/main/java/ro/fastrackit/classroom/Student.java
@@ -0,0 +1,72 @@
+package ro.fastrackit.classroom;
+import java.time.LocalDate;
+
+public class Student {
+    // punctul 1
+    private String lastName;
+    private String firstName;
+    private LocalDate dateOfBirth;
+    private String emailAddress;
+    private Float grade;
+    private Boolean finishedClass;
+
+    //punctul 2
+    public Student(String lastName, String firstName, LocalDate dateOfBirth) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    //punctul 3
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Float grade) {
+        this.grade = grade;
+    }
+
+    public Boolean getFinishedClass() {
+        return finishedClass;
+    }
+
+    public void setFinishedClass(Boolean finishedClass) {
+        this.finishedClass = finishedClass;
+    }
+
+    //punctul 4 encapsulate
+
+}
