15171e8b2dfd54300ee4b8b0a91f56f229a2b2ba
rosca_ioana <rosca.ioana11@gmail.com>
2020-10-17 19:39:46 +0300

    .

--- src/main/java/ro/fastrackit/classroom/ProfessorType.java
+++ src/main/java/ro/fastrackit/classroom/ProfessorType.java
@@ -3,5 +3,5 @@ package ro.fastrackit.classroom;
 public enum ProfessorType implements ScholarType{
     ASSISTANT,
     ASSOCIATE,
-    PROFESSOR;
+    PROFESSOR
 }
--- src/main/java/ro/fastrackit/classroom/StudentType.java
+++ src/main/java/ro/fastrackit/classroom/StudentType.java
@@ -3,5 +3,5 @@ package ro.fastrackit.classroom;
 public enum StudentType implements ScholarType{
     BACHELOR,
     MASTERS,
-    PHD;
+    PHD
 }
