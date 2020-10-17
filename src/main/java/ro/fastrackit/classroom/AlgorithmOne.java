2f5fa806f989be9e9b69a29fbdb3c30ca932332c
rosca_ioana <rosca.ioana11@gmail.com>
2020-10-17 21:44:24 +0300

    .

--- src/main/java/ro/fastrackit/classroom/AlgorithmOne.java
+++ src/main/java/ro/fastrackit/classroom/AlgorithmOne.java
@@ -1,16 +1,18 @@
 package ro.fastrackit.classroom;
 
+import java.util.Scanner;
+
 public class AlgorithmOne {
     public static void main(String[] args) {
-        int rezultat = result(6);
 
-        System.out.println(rezultat);
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter a natural number");
+        int n=scan.nextInt();
+        int m=1;
+
+        for(int i = 1; i<=n; i++){
+            m *= i;
+            System.out.println(m);
+        }
     }
-
-    private static int result(int number) {
-        //TODO
-        return 0;
-    }
-
-
 }
