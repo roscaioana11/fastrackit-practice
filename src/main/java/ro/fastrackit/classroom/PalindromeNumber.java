25375442c940c4ea3a54a27ccf1566c980d51d1e
rosca_ioana <rosca.ioana11@gmail.com>
2020-10-18 17:17:42 +0300

    .

--- src/main/java/ro/fastrackit/classroom/PalindromeNumber.java
+++ src/main/java/ro/fastrackit/classroom/PalindromeNumber.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class PalindromeNumber {
     public static void main(String[] args) {
-        int rem,rev = 0;
+        int rem,rev = 0; //reminder & reverse=0
         Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        int temp = num;
+        int num = scan.nextInt(); //check if the num is palindrome or not
+        int temp = num;  // temp to not have any modifications in the original num
 
         while(temp != 0){
             rem = temp % 10;
             rev = rev * 10 + rem;
             temp = temp / 10;
         }
-        if(rev == num){
+        if(rev == num){ //if true then is palindrome, if false then not palindrome
             System.out.println("Is a Palindrome Number");
         }
         else{
