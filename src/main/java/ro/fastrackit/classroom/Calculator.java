0454006f487e2d3cbda924df8f8581baf94d003a
rosca_ioana <rosca.ioana11@gmail.com>
2020-10-18 22:22:00 +0300

    .

--- src/main/java/ro/fastrackit/classroom/Calculator.java
+++ src/main/java/ro/fastrackit/classroom/Calculator.java
@@ -7,35 +7,31 @@ public class Calculator {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Choose the Operation");
-        System.out.println("1 for Addition");
-        System.out.println("2 for Subtraction");
-        System.out.println("3 for Multiplication");
-        System.out.println("4 for Division");
+        System.out.println("+ for Addition");
+        System.out.println("- for Subtraction");
+        System.out.println("* for Multiplication");
+        System.out.println("/ for Division");
 
-        int choose = scan.nextInt();
+        char choose = scan.next().charAt(0);
 
         System.out.println("Enter Two Numbers");
         int x = scan.nextInt();
         int y = scan.nextInt();
 
-        if(choose == 1){
-            System.out.println("The answer is: " + add(x,y));
+        switch (choose){
+            case '+': System.out.println("The answer is: " + add(x,y));
+                break;
+            case '-': System.out.println("The answer is: " + sub(x,y));
+                break;
+            case '*': System.out.println("The answer is: " + mult(x,y));
+                break;
+            case '/': System.out.println("The answer is: " + div(x,y));
+                break;
+            default: System.out.println("Error! Enter correct operator");
+                return;
 
         }
-        else if(choose == 2){
-            System.out.println("The answer is: " + sub(x,y));
 
-        }
-        else if(choose == 3){
-            System.out.println("The answer is: " + mult(x,y));
-
-        }
-        else if(choose == 4){
-            System.out.println("The answer is: " + div(x,y));
-        }
-        else{
-            System.out.println("Not Available");
-        }
     }
     public static int add(int x, int y){
         int n;
