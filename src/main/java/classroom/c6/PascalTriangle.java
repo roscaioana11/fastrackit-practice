1de0a5825aea3a368817fd3aa452f235198bf5e7
rosca_ioana <rosca.ioana11@gmail.com>
2020-10-31 18:00:46 +0200

    .

---
+++ src/main/java/classroom/c6/PascalTriangle.java
@@ -0,0 +1,30 @@
+package classroom.c6;
+
+import java.util.Scanner;
+
+public class PascalTriangle {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number of lines: ");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int rows = num;
+        int number;
+
+        System.out.println("Triangle: ");
+        for(int i = 0; i<num; i++){
+            //nested for loop for spaces
+            for(int k = 1; k<= rows; k++){
+                System.out.print(" ");
+            }
+            number = 1;
+            for(int j = 0; j<= i; j++){
+                System.out.print(number + " ");
+                number = number * (i-j)/(j+1);
+            }
+            rows--;
+            System.out.println();
+        }
+    }
+}
