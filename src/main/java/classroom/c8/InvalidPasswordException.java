a99476e2c687cb0a3817853fd2d53d57b73cc70e
rosca_ioana <rosca.ioana11@gmail.com>
2020-11-15 17:44:19 +0200

    .

---
+++ src/main/java/classroom/c8/InvalidPasswordException.java
@@ -0,0 +1,214 @@
+package classroom.c8;
+
+/* At least 12characters
+   Uppercase and lowercase
+   Atleast 3 digits
+   Only letters and digits
+
+   If it is valid: the password is printed and the text “is valid”
+   If it is invalid: a custom InvalidPasswordException is thrown with the text containing all rules which are not fulfilled
+ */
+
+
+import java.util.Scanner;
+
+public class InvalidPasswordException extends Exception{
+    int passwordConditionIllegal;
+
+    public static void main(String[] args) {
+
+        Scanner scann = new Scanner (System.in);
+        System.out.print("Password: ");
+
+        while(scann.hasNext()) {
+
+            String pass = scann.nextLine();
+
+            try{
+                System.out.print("Password: " + pass);
+                validatePassCheck(pass);
+                System.out.println(" Valid Password");
+            }catch (InvalidPasswordException caughtInvalidPasswordException){
+                System.err.println(caughtInvalidPasswordException.getMessage());
+                System.out.println(caughtInvalidPasswordException.printMessage());
+            }
+
+        }
+
+    }
+
+    public InvalidPasswordException(int conditionIllegal)
+    {
+        super("Invalid Password");
+        passwordConditionIllegal = conditionIllegal;
+    }
+    public String printMessage()
+    {
+        // Call constructor of parent Exception
+        // according to the illegal condition
+        switch (passwordConditionIllegal) {
+
+            // Password length should be atleat 12 characters
+            case 1:
+                return (" Password length should be atleast 12 characters");
+
+            // Password should not contain any space
+            case 2:
+                return (" Password should not"
+                        + " contain any space");
+
+            // Password should contain// at least 3 digits
+            case 3:
+                return (" Password should contain"
+                        + " at least one digit(0-9)");
+
+            // Password should contain at least one uppercase letter (A-Z)
+            case 4:
+                return (" Password should contain at"
+                        + " least one uppercase letter(A-Z)");
+
+            // Password should contain at least one lowecase letter (a-z)
+            case 5:
+                return (" Password should contain at"
+                        + " least one lowercase letter(a-z)");
+        }
+
+        return ("");
+    }
+
+
+        // whether a password is valid or not
+        public static void validatePassCheck(String password)
+                throws InvalidPasswordException
+        {
+
+            // for checking if password length
+            if (!((password.length() >11))) {
+                throw new InvalidPasswordException(1);
+            }
+
+            // to check space
+            if (password.contains(" ")) {
+                throw new InvalidPasswordException(2);
+            }
+            if (true) {
+                int count = 0;
+
+                // check digits from 0 to 9
+                for (int i = 0; i <= 9; i++) {
+
+                    // to convert int to string
+                    String str1 = Integer.toString(i);
+
+                    if (password.contains(str1)) {
+                        count = 1;
+                    }
+                }
+                if (count == 0) {
+                    throw new InvalidPasswordException(3);
+                }
+            }
+
+            if (true) {
+                int count = 0;
+
+                // checking capital letters
+                for (int i = 65; i <= 90; i++) {
+
+                    // type casting
+                    char c = (char)i;
+
+                    String str1 = Character.toString(c);
+                    if (password.contains(str1)) {
+                        count = 1;
+                    }
+                }
+                if (count == 0) {
+                    throw new InvalidPasswordException(4);
+                }
+            }
+
+            if (true) {
+                int count = 0;
+
+                // checking small letters
+                for (int i = 90; i <= 122; i++) {
+
+                    // type casting
+                    char c = (char)i;
+                    String str1 = Character.toString(c);
+
+                    if (password.contains(str1)) {
+                        count = 1;
+                    }
+                }
+                if (count == 0) {
+                    throw new InvalidPasswordException(5);
+                }
+            }
+
+            // The password is valid
+        }
+
+
+
+/*
+    static void validatePassCheck(String password) throws InvalidPasswordException {
+
+
+
+        int length = 0;                        // Stores the number characters in the password
+        int numCount = 0;                    // Variable used to store numbers in the password
+        int upperCount = 0;                    // Variable used to store capital letters in the password
+        int lowCount = 0;                   // Variable used to store lower letters in the password
+
+        for (int x = 0; x < password.length(); x++) {
+            if ((password.charAt(x) >= 47 && password.charAt(x) <= 58) || (password.charAt(x) >= 64 && password.charAt(x) <= 91) ||
+                    (password.charAt(x) >= 97 && password.charAt(x) <= 122)) {
+                //Keep the Password
+
+                if (password.length() < 12) {                                    // Checks that password is long enough
+                    throw new InvalidPasswordException("Password is Too Short!");
+                }
+
+                for (char passwordDigit : password.toCharArray()) {
+
+                    if (Character.isDigit(passwordDigit)) {                                    // Checks that password contains atleast 3 numbers
+                        throw new InvalidPasswordException("Need atleast 3 numbers" + passwordDigit);
+                    }
+
+
+
+            if ((password.charAt(x) >= 47 && password.charAt(x) <= 58) || (password.charAt(x) >= 64 && password.charAt(x) <= 91) ||
+                    (password.charAt(x) >= 97 && password.charAt(x) <= 122)) {
+                //Keep the Password
+            } else if (password == null || password.isBlank() || password.equals("0")){
+                throw new InvalidPasswordException("Password is missing");		//Checks that password contains only letters and numbers
+            }
+
+            if ((password.charAt(x) > 47 && password.charAt(x) < 58)) {			// Counts the number of numbers
+                numCount ++;
+            }
+
+            if ((password.charAt(x) > 64 && password.charAt(x) < 91)) {			// Counts the number of capital letters
+                upperCount ++;
+            }
+
+            if ((password.charAt(x) > 97 && password.charAt(x) < 122)){
+                lowCount ++;
+            }
+
+            length = (x + 1);								// Counts the passwords length
+
+                }
+
+
+                return;
+
+            }
+        }
+    }
+
+*/
+    }
+
---
+++ src/main/java/classroom/c8/UncheckedPasswordException.java
@@ -0,0 +1,84 @@
+package classroom.c8;
+
+/* At least 12characters
+   Uppercase and lowercase
+   Atleast 3 digits
+   Only letters and digits
+
+   If it is valid: the password is printed and the text “is valid”
+   If it is invalid: a custom InvalidPasswordException is thrown with the text containing all rules which are not fulfilled
+ */
+
+import java.util.Scanner;
+
+public class UncheckedPasswordException{
+    public static void main(String[] args) {
+
+        String pass;
+
+        Scanner scann = new Scanner (System.in);
+
+        System.out.print("Password: ");
+        pass = scann.nextLine();
+
+        System.out.println(validatePassCheck(pass));				// Calls the PassCheck Method on the password entered by the user and prints result to screen
+    }
+
+    static String validatePassCheck(String password){
+
+        String result = "Valid Password";			// Sets the initial result as valid
+        String result2= "Invalid Password";
+        int length = 0;						// Stores the number characters in the password
+        int numCount = 0;					// Variable used to store numbers in the password
+        int upperCount = 0;					// Variable used to store capital letters in the password
+        int lowCount = 0;                   // Variable used to store lower letters in the password
+
+
+        for (int x =0; x < password.length(); x++) {
+            if ((password.charAt(x) >= 47 && password.charAt(x) <= 58) || (password.charAt(x) >= 64 && password.charAt(x) <= 91) ||
+                    (password.charAt(x) >= 97 && password.charAt(x) <= 122)) {
+                //Keep the Password
+            } else {
+                result = "Password Contains Invalid Character!";		//Checks that password contains only letters and numbers
+            }
+
+            if ((password.charAt(x) > 47 && password.charAt(x) < 58)) {			// Counts the number of numbers
+                numCount ++;
+            }
+
+            if ((password.charAt(x) > 64 && password.charAt(x) < 91)) {			// Counts the number of capital letters
+                upperCount ++;
+            }
+
+            if ((password.charAt(x) > 97 && password.charAt(x) < 122)){
+                lowCount ++;
+            }
+
+            length = (x + 1);								// Counts the passwords length
+
+        }
+
+        if (numCount < 3 || upperCount <1 || lowCount <1 || length <12){
+            throw  new RuntimeException(result2 + " Your password needs atleast 12 characters, 3 numbers, 1 Uppercase and 1 Lowercase");
+        }
+/*
+        if (numCount < 3){									// Checks that password contains atleast 3 numbers
+            throw new RuntimeException(result2 + ". Need atleast 3 numbers.");
+        }
+
+        if (upperCount < 1) {									// Checks that the password contains atleast 1 uppercase letter
+            throw new RuntimeException(result2 + ". Not Enough Capital Letters in the Password!");
+        }
+
+        if(lowCount < 1){                                   //Checks that password contains atleast 1 lowercase letter
+            throw new RuntimeException(result2 + ". Not enough Lower Letters in the Password.");
+        }
+
+        if (length < 12){									// Checks that password is long enough
+            throw new RuntimeException(result2 + ". Password is Too Short!");
+        }
+*/
+        return (result);
+
+    }
+}
